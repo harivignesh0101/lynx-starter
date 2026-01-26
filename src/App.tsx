95a587952ff4a10ab4875a04b7ecb75087037535
Harivignesh <33279379+harivignesh0101@users.noreply.github.com>
2026-01-26 10:04:23 +0530

    Update title text from 'React test 9' to 'React test 10'

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ export function App() {
               ? <image src={reactLynxLogo} className='Logo--react' />
               : <image src={lynxLogo} className='Logo--lynx' />}
           </view>
-          <text className='Title'>React test 9</text>
+          <text className='Title'>React test 10</text>
           <text className='Subtitle'>on Lynx app</text>
         </view>
         <view className='Content'>
